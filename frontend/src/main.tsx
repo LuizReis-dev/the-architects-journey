2136457bf3bb98ff3c280116206059e9e4caa72f
Luiz Reis <reisluiz05@gmail.com>
2026-07-12 12:32:37 -0300

    feat: adding design system css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './router';
+import './styles/design-system.css';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
